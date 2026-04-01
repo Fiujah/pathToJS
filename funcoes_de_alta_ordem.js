20fcf8939d0e8155513f63b5963579afff3b645f
Renan Fiuja <144910478+Fiujah@users.noreply.github.com>
2026-04-01 09:54:27 +0000

    adicionando arquivos no commit do vscode web

---
+++ funcoes_de_alta_ordem.js
@@ -0,0 +1,19 @@
+// Funções de Alta Ordem
+// - Recebem uma ou mais funções como argumento
+
+function aplicarOperacao(x, operacao) { return operacao(x); }
+function dobrar(numero) { return numero * 2; };
+const resultado = aplicarOperacao(5, dobrar)
+console.log(resultado)
+
+// - Recebem uma função como resultado
+
+function criarIncrementador(incremento) {
+    return function(numero) {
+        return numero + incremento;
+    }
+}
+
+const incrementarPor2 = criarIncrementador(2)
+
+console.log(incrementarPor2(5))
