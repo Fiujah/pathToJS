2534482bdacce302b02f944f8c64d04eb7a794ee
Renan Fiuja <144910478+Fiujah@users.noreply.github.com>
2026-04-04 10:48:20 +0000

    salvando os comandos do git pra não esquecer

---
+++ comandos_git_hub.bash
@@ -0,0 +1,3 @@
+git add . # pra adicionar os arquivos no commit que vai subir as atualizações, pode colocar só um arquivo ao invés do .
+git commit -m "mensagem pra descrever o commit"
+git push # sobe de fato o commit pro github
